9ef697ef3ef6a198873c8bad89546d6759b2da21
Lucy Beck <lucywbeck@Lucys-Air-2.attlocal.net>
2023-02-12 15:23:22 -0600

    add navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { Container, Nav, Navbar, NavDropdown, Image } from "react-bootstrap";
 import { useDbData } from "../../utilities/firebase";
 
 export default function NavbarApp() {
@@ -5,10 +7,25 @@ export default function NavbarApp() {
     if (data !== undefined) {
         console.log("database", data);
     }
-
     return (
-        <>
-        <h1>Hello there!!!</h1>
-        </>
-    )
+        <div>
+        <Navbar collapseOnSelect bg="dark" variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand href="/" data-cy="pageTitle">Itinerary Planner</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                    className="me-auto my-2 my-lg-0"
+                    style={{ maxHeight: "100px" }}
+                    navbarScroll
+                    >
+                        <Nav.Link href="/" data-cy="home">Home</Nav.Link>
+                        <Nav.Link href="/" data-cy="home">Locations</Nav.Link>
+                        <Nav.Link href="/" data-cy="home">Adventures</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        </div>
+    );
 }
