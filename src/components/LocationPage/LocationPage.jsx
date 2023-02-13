24663d9ffa96cefe72cc6ba05497a601dcf563ce
Karthik Subramanian <thikkar@karthiks-air.ads.northwestern.edu>
2023-02-13 12:01:42 -0600

    fixed card rendering issue

--- src/components/LocationPage/LocationPage.jsx
+++ src/components/LocationPage/LocationPage.jsx
@@ -4,29 +4,23 @@ import { useDbData } from "../../utilities/firebase";
 import LocationCard from "../LocationCard/LocationCard";
 
 export default function LocationPage() {
-
     const [data, error] = useDbData();
     const [locationData, setLocationData] = useState([])
     const city = "Paris"
 
     useEffect(() => {
         if (data) setLocationData(Object.values(data.locations[city]))
-        console.log(data)
-        console.log(locationData)
     }, [data])
 
     if (!data) {
         return <p>Loading</p>
     }
 
-    console.log(Object.values(data.locations[city]))
-
     return (
        <div>
-        {locationData.map((location, idx) => {
+        {locationData.map((location, idx) => (
             <LocationCard key={idx} location={location} />
-        })}
-        {/* <LocationCard location={Object.values(data.locations[city])[0]}/> */}
+        ))}
        </div>
     );
 }
