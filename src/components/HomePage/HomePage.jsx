28b6e566557cb7a526523bec7bfe56db3a8732c5
Lucy Beck <lucywbeck@Lucys-Air-2.attlocal.net>
2023-02-15 22:10:32 -0600

    add src/components/TimeLeft/

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,10 +1,11 @@
 import AdventurePage from "../AdventurePage/AdventurePage"
 import LocationPage from "../LocationPage/LocationPage"
-
+import TimeLeft from "../TimeLeft/TimeLeft"
 
 export default function HomePage() {
     return (
         <>
+        <TimeLeft />
         <AdventurePage />
         <LocationPage city="Paris"/>
         </>
---
+++ src/components/TimeLeft/TimeLeft.jsx
@@ -0,0 +1,10 @@
+import './TimeLeft.css'
+
+export default function TimeLeft() {
+    
+    return (
+        <>
+        <h1 className="time-left">Based on your selections <br/> you have 3 hours left</h1>
+        </>
+    )
+}
