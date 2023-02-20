0e9fbfd04d53722681096463740426b7f157fc55
IsaiasLipa <>
2023-02-20 16:45:55 -0600

    go button

--- src/components/AdventurePage/AdventurePage.jsx
+++ src/components/AdventurePage/AdventurePage.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Container, Nav, Navbar, NavDropdown, Image } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown, Image, Button } from "react-bootstrap";
 import { useDbData } from "../../utilities/firebase";
 import AdventureCard from "../AdventureCard/AdventureCard";
 import WaypointMap from "../AdventurePage/WaypointMap";
 import '../AdventurePage/AdventurePage.css'
+import { useNavigate } from "react-router-dom";
 
 
 export default function AdventurePage() {
     const [data, error] = useDbData();
     const [selectedLocations, setSelectedLocations] = useState([])
     const user = "user1"
-
+    const navigate = useNavigate();
     useEffect(() => {
         if (data) {
             if (data.users[user]["adventure"]["selectedLocations"]) {
@@ -24,12 +25,14 @@ export default function AdventurePage() {
     if (!data) {
         return <p>Loading</p>
     }
-    // console.log("selectedLocations", selectedLocations)
+    const handleGoClick = () => {
+        navigate("/go");
+    }
     return (
         <>
             <h3 style={{padding:"10px"}}>My Selected Options</h3>
             <div className="adventure-cards">
-            {/* <WaypointMap selectedLocations={selectedLocations}/> */}
+            <Button  onClick={handleGoClick} variant="success">Go</Button>
             {selectedLocations.length > 0 && selectedLocations.map((location) => (
                 <div key={location.id}>
                     <AdventureCard location={location} />
--- src/components/AdventurePage/WaypointMap.jsx
+++ src/components/AdventurePage/WaypointMap.jsx
@@ -19,40 +19,33 @@ const WaypointMap=  () => {
     if (!data) {
         return <p>Loading</p>
     }
-    console.log("selectedLocations", selectedLocations)
-
+    const waypoints = selectedLocations.slice(0, -1).map(str => `'${str.address}'`).join(' | ');
     
-    
+    return(
+        <div class= "map div" style={{"height": "100%"}}>
+        {selectedLocations=== undefined || selectedLocations.length ==0 ? <div> No Locations added</div> : 
+        <div>
+            {selectedLocations.length ==1 ? 
+                <iframe
+                        width = "100%"
+                        height="100%"
+                        src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyAres6dxJqN_EEzqHrFIXPHg4tGVuSLERA&origin="13 Rue du Mail, 75002 Paris, France"&destination=${selectedLocations[0].address}&mode=walking`}
+                >       
+                </iframe>
+            : 
+                <iframe
+                        width = "100%"
+                        height= "100%"
+                        src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyAres6dxJqN_EEzqHrFIXPHg4tGVuSLERA&origin="13 Rue du Mail, 75002 Paris, France"&destination=${selectedLocations[selectedLocations.length-1].address}&waypoints=${waypoints}&mode=walking`}
+                >       
+                </iframe>
+            }
+        </div>
 
-    //const waypoints = selectedLocations.slice(0, -1).map(str => `'${str.address}'`).join(' | ');
-    
-    // return(
-    //     <div>
-    //     {selectedLocations=== undefined || selectedLocations.length ==0 ? <div></div> : 
-    //     <div>
-    //         {selectedLocations.length ==1 ? 
-    //             <iframe
-    //                     width = "100%"
-    //                     height="300px"
-    //                     src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyAres6dxJqN_EEzqHrFIXPHg4tGVuSLERA&origin="13 Rue du Mail, 75002 Paris, France"&destination=${selectedLocations[0].address}&mode=walking`}
-    //             >       
-    //             </iframe>
-    //         : 
-    //             <iframe
-    //                     width = "100%"
-    //                     height="300px"
-    //                     src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyAres6dxJqN_EEzqHrFIXPHg4tGVuSLERA&origin="13 Rue du Mail, 75002 Paris, France"&destination=${selectedLocations[selectedLocations.length-1].address}&waypoints=${waypoints}&mode=walking`}
-    //             >       
-    //             </iframe>
-    //         }
-    //     </div>
-
-    // }
-    //     </div>
-    // )
-    return (
-        <div> Test</div>
+    }
+        </div>
     )
+    
 }
 
 
