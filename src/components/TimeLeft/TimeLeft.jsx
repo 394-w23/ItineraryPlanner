4dc423674cc39546845d049f1ccdc2f23a954f24
Aziz Jalel <azizjalelcontact@gmail.com>
2023-02-20 16:16:38 -0600

    timer

--- src/components/TimeLeft/TimeLeft.jsx
+++ src/components/TimeLeft/TimeLeft.jsx
@@ -1,10 +1,24 @@
 import './TimeLeft.css'
+import ProgressTimer from 'react-progress-bar-timer';
 
+const TimerComponent = () => (
+    
+<ProgressTimer
+  direction="left"
+  duration={120}
+  label="3 hours left"
+  onFinish={function noRefCheck(){}}
+  rootRounded
+  started={true}
+  variant="fill"
+ />
+  );
 export default function TimeLeft() {
     
     return (
         <div className="time-left-banner">
         <h4 className="time-left">Based on your selections you have 3 hours left</h4>
+        <TimerComponent />
         </div>
     )
 }
