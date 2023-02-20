3783ea3f0787991d180ac1e329962725c7e47e4c
IsaiasLipa <>
2023-02-20 15:56:39 -0600

    first page

--- src/components/AdventurePage/WaypointMap.jsx
+++ src/components/AdventurePage/WaypointMap.jsx
@@ -1,29 +1,43 @@
-const WaypointMap= ({ selectedLocations }) => {
-    const waypoints = selectedLocations.slice(0, -1).map(str => `'${str.address}'`).join(' | ');
-    
-    return(
-        <div>
-        {selectedLocations=== undefined || selectedLocations.length ==0 ? <div></div> : 
-        <div>
-            {selectedLocations.length ==1 ? 
-                <iframe
-                        width = "100%"
-                        height="300px"
-                        src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyAres6dxJqN_EEzqHrFIXPHg4tGVuSLERA&origin="13 Rue du Mail, 75002 Paris, France"&destination=${selectedLocations[0].address}&mode=walking`}
-                >       
-                </iframe>
-            : 
-                <iframe
-                        width = "100%"
-                        height="300px"
-                        src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyAres6dxJqN_EEzqHrFIXPHg4tGVuSLERA&origin="13 Rue du Mail, 75002 Paris, France"&destination=${selectedLocations[selectedLocations.length-1].address}&waypoints=${waypoints}&mode=walking`}
-                >       
-                </iframe>
-            }
-        </div>
+import { getData, updateDatabase } from "../../utilities/firebase";
 
+const WaypointMap= async () => {
+    let selectedLocations = await getData("users/user1/adventure/selectedLocations");
+    
+
+    if (!selectedLocations) {
+        return <h1>loading</h1>
+        //const waypoints = selectedLocations.slice(0, -1).map(str => `'${str.address}'`).join(' | ');
     }
-        </div>
+    console.log(selectedLocations)
+
+    //const waypoints = selectedLocations.slice(0, -1).map(str => `'${str.address}'`).join(' | ');
+    
+    // return(
+    //     <div>
+    //     {selectedLocations=== undefined || selectedLocations.length ==0 ? <div></div> : 
+    //     <div>
+    //         {selectedLocations.length ==1 ? 
+    //             <iframe
+    //                     width = "100%"
+    //                     height="300px"
+    //                     src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyAres6dxJqN_EEzqHrFIXPHg4tGVuSLERA&origin="13 Rue du Mail, 75002 Paris, France"&destination=${selectedLocations[0].address}&mode=walking`}
+    //             >       
+    //             </iframe>
+    //         : 
+    //             <iframe
+    //                     width = "100%"
+    //                     height="300px"
+    //                     src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyAres6dxJqN_EEzqHrFIXPHg4tGVuSLERA&origin="13 Rue du Mail, 75002 Paris, France"&destination=${selectedLocations[selectedLocations.length-1].address}&waypoints=${waypoints}&mode=walking`}
+    //             >       
+    //             </iframe>
+    //         }
+    //     </div>
+
+    // }
+    //     </div>
+    // )
+    return (
+        <div> Test</div>
     )
 }
 
