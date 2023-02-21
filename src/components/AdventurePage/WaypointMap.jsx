c3aebab72a5eef1ed23ad794a60036ac08e4c51a
Aziz Jalel <azizjalelcontact@gmail.com>
2023-02-20 21:53:38 -0600

    Adding timer

--- src/components/AdventurePage/WaypointMap.jsx
+++ src/components/AdventurePage/WaypointMap.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDbData } from "../../utilities/firebase";
+import ProgressTimer from 'react-progress-bar-timer';
+
 
 const WaypointMap=  () => {
     const [data, error] = useDbData();
@@ -22,19 +24,35 @@ const WaypointMap=  () => {
     const waypoints = selectedLocations.slice(0, -1).map(str => `'${str.address}'`).join(' | ');
     
     return(
+        
         <div class= "map div" style={{"height": "100%"}}>
+            <ProgressTimer
+  barRounded
+  buttonText=""
+  classes={{}}
+  fontSize={15}
+  color="#2f2d2d"
+  duration={60}
+  fontColor="#ffffff"
+  label="Time remaining
+"
+  onFinish={function noRefCheck(){}}
+  showDuration
+  started
+  variant="fill"
+/>
         {selectedLocations=== undefined || selectedLocations.length ==0 ? <div> No Locations added</div> : 
-        <div>
+        <div style={{"height": "60em"}}>
             {selectedLocations.length ==1 ? 
                 <iframe
-                        width = "auto"
+                        width = "100%"
                         height="100%"
                         src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyAres6dxJqN_EEzqHrFIXPHg4tGVuSLERA&origin="13 Rue du Mail, 75002 Paris, France"&destination=${selectedLocations[0].address}&mode=walking`}
                 >       
                 </iframe>
             : 
                 <iframe
-                        width = "auto"
+                        width = "100%"
                         height= "100%"
                         src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyAres6dxJqN_EEzqHrFIXPHg4tGVuSLERA&origin="13 Rue du Mail, 75002 Paris, France"&destination=${selectedLocations[selectedLocations.length-1].address}&waypoints=${waypoints}&mode=walking`}
                 >       
