65139b3148fd1de9177a135492928676fd97e93b
Aziz Jalel <azizjalelcontact@gmail.com>
2023-02-27 16:47:31 -0600

    Auth test

---
+++ tests/Auth.test.jsx
@@ -0,0 +1,16 @@
+// add a test to see if the user is logged in
+import { describe, expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { AuthProvider } from '../utilities/firebase';
+import App from '../App';
+
+describe('Auth tests', () => {
+    test('User should be logged in', () => {
+        render(
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        );
+        expect(screen.getByText('Log in')).toBeDefined();
+    });
+});
