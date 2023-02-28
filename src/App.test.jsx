a6f0d43fd326d980e3ca319087d099377516b5b1
patrickhoey20 <patrickhoey2024@u.northwestern.edu>
2023-02-27 21:20:22 -0600

    initial vitest

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,12 +1,12 @@
 import {describe, expect, test} from 'vitest';
 import {fireEvent, render, screen} from '@testing-library/react';
-import App from './App';
+import Root from './Root';
 
 describe('Home page tests', () => {
     
-  test("User prompt should load onto the home page", () => {
-    render(<App />);
-    expect(screen.getByText("Start building your adventure")).toBeDefined();  
+  test("Start My Adventure button should load onto the home page", () => {
+    render(<Root />);
+    expect(screen.getByText("Start My Adventure")).toBeDefined();  
   });
 
 });
