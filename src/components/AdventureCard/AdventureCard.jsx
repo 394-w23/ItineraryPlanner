07a01e4b13e55951d8d436d2c63a0f0512da0fde
SengdowJones <sengdao.inthavong3@gmail.com>
2023-02-27 22:42:38 -0600

    Implement side button and icons

--- src/components/AdventureCard/AdventureCard.jsx
+++ src/components/AdventureCard/AdventureCard.jsx
@@ -1,64 +1,74 @@
-import React from 'react'
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { getData, updateDatabase } from "../../utilities/firebase";
-import '../AdventureCard/AdventureCard.css'
-
+import { getData, updateDatabase } from '../../utilities/firebase';
+import './AdventureCard.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
 const AdventureCard = ({ location }) => {
-  // console.log("location", location)
-
   const saveLocation = async () => {
-    const locationsUpdated = {}
-    let locations = await getData("users/user1/adventure/locations");
-    
-    if (locations) {
-      var index = locations.map(function(location) { return location["name"]; }).indexOf(location["name"]);
-      locations[index]["selected"] = true;
+    const locationsUpdated = {};
+    let locations = await getData('users/user1/adventure/locations');
 
-      locationsUpdated["users/user1/adventure/locations"] = locations;
+    if (locations) {
+      var index = locations
+        .map(function (location) {
+          return location['name'];
+        })
+        .indexOf(location['name']);
+      locations[index]['selected'] = true;
+
+      locationsUpdated['users/user1/adventure/locations'] = locations;
       updateDatabase(locationsUpdated);
     }
   };
 
   const removeLocation = async () => {
-    const locationsUpdated = {}
-    let locations = await getData("users/user1/adventure/locations");
-    
-    if (locations) {
-      var index = locations.map(function(location) { return location["name"]; }).indexOf(location["name"]);
-      locations[index]["selected"] = false;
+    const locationsUpdated = {};
+    let locations = await getData('users/user1/adventure/locations');
 
-      locationsUpdated["users/user1/adventure/locations"] = locations;
+    if (locations) {
+      var index = locations
+        .map(function (location) {
+          return location['name'];
+        })
+        .indexOf(location['name']);
+      locations[index]['selected'] = false;
+
+      locationsUpdated['users/user1/adventure/locations'] = locations;
       updateDatabase(locationsUpdated);
     }
   };
 
   return (
-    <>
-    { location["selected"] ? 
-    <Card className="selected">
-      <Card.Img variant="top" src={location.image} />
-      <Card.Body>
-        <Card.Title>{location.name}</Card.Title>
-        <Card.Text>Suggested Time: {location.suggestedTime ? location.suggestedTime : 0} Hr</Card.Text>
-        {!location["startOrEnd"] && <Button variant="primary" onClick={removeLocation}>Remove from adventure</Button>}
-      </Card.Body>
-    </Card>
-    :
-    <Card className="unselected">
-      <Card.Img variant="top" src={location.image} />
-      <Card.Body>
-        <Card.Title>{location.name}</Card.Title>
-        <Card.Text>Suggested Time: {location.suggestedTime ? location.suggestedTime : 0} Hr</Card.Text>
-        {!location["startOrEnd"] && <Button variant="primary" onClick={saveLocation}>Add to adventure</Button>}
-      </Card.Body>
-    </Card>
-    }
+    <div className="card-container">
+      <div className="button-sidebar">
+        {!location['startOrEnd'] && (
+          <>
+            {location['selected'] ? (
+              <Button className="adventure-button" variant="danger" onClick={removeLocation}>
+                <FontAwesomeIcon icon={faMinus} />
+              </Button>
+            ) : (
+              <Button className="adventure-button" variant="success" onClick={saveLocation}>
+                <FontAwesomeIcon icon={faPlus} />
+              </Button>
+            )}
+          </>
+        )}
+      </div>
+      <Card className="adventure-card">
+        <Card.Img variant="top" src={location.image} />
+        <Card.Body className="adventure-card-body">
+          <Card.Title>{location.name}</Card.Title>
+          <Card.Text>
+            Suggested Time: {location.suggestedTime ? location.suggestedTime : 0} Hr
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
 
-    </>
-    
-  )
-}
-
-export default AdventureCard
+export default AdventureCard;
