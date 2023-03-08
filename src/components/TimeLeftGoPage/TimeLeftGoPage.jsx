67cdbeaaaf8b5afcc76fa1c9aa7eb5958c96243b
TimHuynh0905 <timhuynh0905@gmail.com>
2023-03-07 21:06:12 -0600

    feat: timer coutdown

--- src/components/TimeLeftGoPage/TimeLeftGoPage.jsx
+++ src/components/TimeLeftGoPage/TimeLeftGoPage.jsx
@@ -1,36 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { useDbData } from "../../utilities/firebase";
-import { Subject } from 'rxjs';
-import './TimeLeft.css'
-import ProgressTimer from 'react-progress-bar-timer';
+import { Subject } from "rxjs";
+import "./TimeLeft.css";
+import ProgressTimer from "react-progress-bar-timer";
 
 const TimerComponent = () => (
-    
-<ProgressTimer
-  direction="left"
-  duration={500}
-  label="3 hours left"
-  onFinish={function noRefCheck(){}}
-  rootRounded
-  started={true}
-  variant="fill"
- />
-  );
+  <ProgressTimer
+    direction="left"
+    duration={500}
+    label="3 hours left"
+    onFinish={function noRefCheck() {}}
+    rootRounded
+    started={true}
+    variant="fill"
+  />
+);
 
-// 
-const subject = new Subject()
+//
+const subject = new Subject();
 export const remainingTimeService = {
-    setRemainingTime: t => subject.next({ value: t }),
-    clearRemainingTime: () => subject.next(),
-    getRemainingTime: () => subject.asObservable()
-}
+  setRemainingTime: (t) => subject.next({ value: t }),
+  clearRemainingTime: () => subject.next(),
+  getRemainingTime: () => subject.asObservable(),
+};
 
 export default function TimeLeftGoPage() {
-    // hard code 5.5 hours
-    
-    return (
-        <div className="time-left-banner">
-            <div className="time-left">Adventure Progress: 05:24:12</div>
-        </div>
-    )
+  const [duration, setDuration] = useState(0);
+
+  const formattedDuration = useMemo(() => {
+    const seconds = Math.floor((duration / 1000) % 60);
+    const minutes = Math.floor((duration / (1000 * 60)) % 60);
+    const hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    return `${hours}:${minutes}:${seconds}`;
+  }, [duration]);
+
+  useEffect(() => {
+    const duration = 5 * 60 * 60 * 1000 + 30 * 60 * 1000;
+    const startTime = Date.now() + duration;
+    const interval = setInterval(
+      () => setDuration(startTime - Date.now()),
+      1000
+    );
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="time-left-banner">
+      <div className="time-left">Adventure Progress: {formattedDuration}</div>
+    </div>
+  );
 }
